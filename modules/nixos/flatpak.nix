bd748dae37fb3c0272817836d50e912382b558d9
Wyatt Gill <101477459+wyattgill9@users.noreply.github.com>
2026-05-19 19:16:42 -0700

    fix: removed sober

--- modules/nixos/flatpak.nix
+++ modules/nixos/flatpak.nix
@@ -3,12 +3,12 @@
   services.flatpak = {
     enable = true;
 
-    remotes = lib.mkOptionDefault [
-      {
-        name = "flathub-beta";
-        location = "https://flathub.org/beta-repo/flathub-beta.flatpakrepo";
-      }
-    ];
+    # remotes = lib.mkOptionDefault [
+    #   {
+    #     name = "flathub-beta";
+    #     location = "https://flathub.org/beta-repo/flathub-beta.flatpakrepo";
+    #   }
+    # ];
 
     update.auto.enable = false;
     uninstallUnmanaged = false;

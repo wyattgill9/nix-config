f6ebbc337bb93ae325510b10ef21d838317d9930
Wyatt Gill <101477459+wyattgill9@users.noreply.github.com>
2026-05-19 17:16:18 -0700

    fix: readd tmux for nixos host

--- modules/home/terminal/ghostty.nix
+++ modules/home/terminal/ghostty.nix
@@ -20,4 +20,18 @@ _: {
       gtk-single-instance = true;
     };
   };
+
+  programs.tmux = {
+    enable = true;
+    clock24 = true;
+    baseIndex = 1;
+
+    extraConfig = ''
+      set -g default-terminal "tmux-256color"
+      set -s escape-time 0
+
+      set -g status-style "fg=white,bg=black"
+      set-option -g status-position top
+    '';
+  };
 }
